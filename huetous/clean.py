dc33d22d4fad90153f400e9700dd7a5bbde80fca
daddudota22 <daddudota22@mail.ru>
2019-07-07 19:00:40 +0700

    + StackingAveragedModels, show_skewed_feats

--- huetous/clean.py
+++ huetous/clean.py
@@ -6,16 +6,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import re
 
+# Categories cleaning
+# Categories concatenation
 
 class Cleaner():
     def __init__(self, df):
         self.df = df.copy(deep=True)
 
+    def return_df(self):
+        return self.df
+
     def do_fillna_median(self, col, astype=np.int16):
-        self.df[col].fillna(df[col].median(), inplace=True).astype(astype)
+        self.df[col].fillna(self.df[col].median(), inplace=True).astype(astype)
 
     def do_fillna_mode(self, col, astype=np.int16):
-        self.df[col].fillna(df[col].mode()[0], inplace=True).astype(astype)
+        self.df[col].fillna(self.df[col].mode()[0], inplace=True).astype(astype)
 
     def do_fillna_meanstd(self, col, astype=np.int16):
         age_avg = self.df[col].mean()
@@ -23,11 +28,11 @@ class Cleaner():
         age_null_count = self.df[col].isnull().sum()
         age_null_random_list = \
             np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
-        self.df[col][np.isnan(df[col])] = age_null_random_list
-        self.df[col] = self.df[col].astype(vartype)
+        self.df[col][np.isnan(self.df[col])] = age_null_random_list
+        self.df[col] = self.df[col].astype(astype)
 
     def do_mapping(self, col, map, astype=np.int16):
-        self.df[col] = self.df[col].map(map).astype(var_type)
+        self.df[col] = self.df[col].map(map).astype(astype)
 
     def do_reduce_memory_usage(self, verbose=True):
         numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
@@ -54,7 +59,8 @@ class Cleaner():
                     else:
                         self.df[col] = self.df[col].astype(np.float64)
         end_mem = self.df.memory_usage().sum() / 1024 ** 2
-        if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (
+        if verbose:
+            print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (
                 start_mem - end_mem) / start_mem))
 
     def do_was_missing_cols(self):
@@ -88,105 +94,15 @@ class Cleaner():
         print('Number of duplicates:', len(duplicates))
         print(duplicates)
 
-# Fillna
-# Categories cleaning
-# Categories concatenation
+    def show_skewed(self):
+        sk_df = pd.DataFrame([{'column': col, 'uniq': self.df[col].nunique(),
+                               'skewness': self.df[col].value_counts(normalize=True).values[0] * 100} for col in
+                              self.df.columns])
+        sk_df = sk_df.sort_values('skewness', ascending=False)
+        print('Skewed features: \n')
+        print(sk_df)
 
 
 
-# # --------------------------------------------------------------------------------------------
-# def do_fillna(dataset, column, type, vartype=int):
-#     if type == 'median':
-#         dataset[column].fillna(dataset[column].median(), inplace=True)
-#     if type == 'mode':
-#         dataset[column].fillna(dataset[column].mode()[0], inplace=True)
-#     if type == 'meanstd':
-#         age_avg = dataset[column].mean()
-#         age_std = dataset[column].std()
-#         age_null_count = dataset[column].isnull().sum()
-#         age_null_random_list = \
-#             np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
-#         dataset[column][np.isnan(dataset[column])] = age_null_random_list
-#         dataset[column] = dataset[column].astype(vartype)
-#
-#
-# # --------------------------------------------------------------------------------------------
-# def do_mapping(dataframe, col, map, var_type):
-#     dataframe[col] = dataframe[col].map(map).astype(var_type)
-#
-
-# --------------------------------------------------------------------------------------------
-# def do_reduce_memory_usage(df, verbose=True):
-#     numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
-#     start_mem = df.memory_usage().sum() / 1024 ** 2
-#     for col in df.columns:
-#         col_type = df[col].dtypes
-#         if col_type in numerics:
-#             c_min = df[col].min()
-#             c_max = df[col].max()
-#             if str(col_type)[:3] == 'int':
-#                 if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-#                     df[col] = df[col].astype(np.int8)
-#                 elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-#                     df[col] = df[col].astype(np.int16)
-#                 elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-#                     df[col] = df[col].astype(np.int32)
-#                 elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-#                     df[col] = df[col].astype(np.int64)
-#             else:
-#                 if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
-#                     df[col] = df[col].astype(np.float16)
-#                 elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
-#                     df[col] = df[col].astype(np.float32)
-#                 else:
-#                     df[col] = df[col].astype(np.float64)
-#     end_mem = df.memory_usage().sum() / 1024 ** 2
-#     if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (
-#             start_mem - end_mem) / start_mem))
-#     return df
 
 
-# --------------------------------------------------------------------------------------------
-# --------------------------------------------------------------------------------------------
-# Create new column which will tell what value was missed
-# --------------------------------------------------------------------------------------------
-# def do_was_missing_cols(df):
-#     df_copy = df.copy()
-#
-#     cols_with_missing = [col for col in df.columns if df[col].isnull().any()]
-#     for col in cols_with_missing:
-#         df_copy[col + "_was_missing"] = df_copy[col].isnull()
-#
-#     imp_df_copy = pd.DataFrame(SimpleImputer().fit_transform(df_copy))
-#     imp_df_copy.columns = df_copy.columns
-#     return imp_df_copy
-
-
-# --------------------------------------------------------------------------------------------
-# def show_missing_data(dataframe, plot=False):
-#     feats_name = []
-#     feats_percent = []
-#     percents = (dataframe.isnull().sum() / dataframe.isnull().count()).sort_values(ascending=False)
-#
-#     print('Missing data')
-#     for index in percents.index:
-#         if percents[index] != 0:
-#             feats_name.append(index)
-#             feats_percent.append(percents[index])
-#             print('{} : {:.5f}'.format(index, percents[index]))
-#     print("\n")
-#     if plot:
-#         f, ax = plt.subplots(figsize=(9, 6))
-#         plt.xticks(rotation='90')
-#         sns.barplot(x=feats_name, y=feats_percent)
-#         plt.xlabel('Features', fontsize=15)
-#         plt.ylabel('Percent of missing values', fontsize=15)
-#         plt.title('Percent missing data by feature', fontsize=15)
-#         plt.show()
-#
-#
-# # --------------------------------------------------------------------------------------------
-# def show_duplicates(df):
-#     duplicates = df[df.duplicated()]
-#     print('Number of duplicates:', len(duplicates))
-#     print(duplicates)
--- huetous/ensemble.py
+++ huetous/ensemble.py
@@ -1,10 +1,12 @@
 from sklearn import svm, tree, linear_model, neighbors, naive_bayes, ensemble, discriminant_analysis, gaussian_process
 from xgboost import XGBClassifier, XGBRegressor
 from sklearn import model_selection
+from sklearn.base import BaseEstimator, RegressorMixin, TransformerMixin
 import pandas as pd
 from sklearn.base import clone
 import numpy as np
 from sklearn.model_selection import KFold
+
 
 # --------------------------------------------------------------------------------------------
 def get_models(regression=True, names=None):
@@ -180,7 +182,7 @@ def get_models(regression=True, names=None):
 
 
 # --------------------------------------------------------------------------------------------
-class AveragingModels():
+class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
     def __init__(self, models):
         self.models = models
 
@@ -193,27 +195,44 @@ class AveragingModels():
         return self
 
     def predict(self, X):
-        preds = np.column_stack([
+        predictions = np.column_stack([
             model.predict(X) for model in self.models_
         ])
-        return np.mean(preds, axis=1)
+        return np.mean(predictions, axis=1)
 
 
 # --------------------------------------------------------------------------------------------
-class StackingAveragedModels():
-    def __init__(self, base_models, meta_model, n_folds=5):
+class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, base_models, meta_model, n_folds=5, shuffle=True):
         self.base_models = base_models
         self.meta_model = meta_model
         self.n_folds = n_folds
+        self.shuffle =shuffle
 
-    def fit(self, X, y, seed=156):
-        self.base_models_ = [clone(x) for x in self.base_models]
+    def fit(self, X, y):
+        self.base_models_ = [list() for x in self.base_models]
         self.meta_model_ = clone(self.meta_model)
-        kfold = KFold(n_splits=self.n_folds, shuffle=True, random_state=seed)
+        kfold = KFold(n_splits=self.n_folds, shuffle=self.shuffle, random_state=156)
 
-        # oof
-        # self.meta_model_.fit(oof_preds, y)
+        oof_pred = np.zeros((X.shape[0], len(self.base_models)))
+        for i, model in enumerate(self.base_models):
+            for tr_idx, te_idx in kfold.split(X, y):
+                instance = clone(model)
+                self.base_models_[i].append(instance)
+
+                instance.fit(X[tr_idx], y[tr_idx])
+                y_pred = instance.predict(X[te_idx])
+
+                oof_pred[te_idx, i] = y_pred
+
+        self.meta_model_.fit(oof_pred, y)
         return self
+
+    def predict(self, X):
+        meta_features = np.column_stack([
+            np.column_stack([model.predict(X) for model in base_models]).mean(axis=1)
+            for base_models in self.base_models_])
+        return self.meta_model_.predict(meta_features)
 
 
 # --------------------------------------------------------------------------------------------
